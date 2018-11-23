e984ab32469c0818d74b4b14464085c14a944d7c
Iz <irzhy@wau.co>
2018-11-23 22:46:48 +0300

    fix usernames endpoint

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -187,7 +187,7 @@ class UserModel extends AbstractModel
      */
     public function getUsersByUsernames(array $requestOptions)
     {
-        return $this->client->post(self::$endpoint . '/usernames/', $requestOptions);
+        return $this->client->post(self::$endpoint . '/usernames', $requestOptions);
     }
 
     /**
